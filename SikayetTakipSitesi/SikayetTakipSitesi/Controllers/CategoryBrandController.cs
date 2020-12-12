38990f656a4ae2626387bd55a449c04da0aa0a2e
Hakan Durgay <hakandurgay@gmail.com>
2020-12-12 22:28:06 +0300

    markalar sayfası eklendi

--- SikayetTakipSitesi/SikayetTakipSitesi/Controllers/CategoryBrandController.cs
+++ SikayetTakipSitesi/SikayetTakipSitesi/Controllers/CategoryBrandController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SikayetTakipSitesi.Data;
 using SikayetTakipSitesi.Models;
+using SikayetTakipSitesi.ViewModels;
 
 namespace SikayetTakipSitesi.Controllers
 {
@@ -14,153 +15,33 @@ namespace SikayetTakipSitesi.Controllers
     {
         private readonly SikayetDbContext _context;
 
+
         public CategoryBrandController(SikayetDbContext context)
         {
             _context = context;
         }
 
-        // GET: CategoryBrand
+
+        CategoryBrandModelView categoryBrandViewModel = new CategoryBrandModelView();
+
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var sikayetDbContext = _context.CategoryBrands.Include(c => c.Brand).Include(c => c.Category);
-            return View(await sikayetDbContext.ToListAsync());
+            //   categoryBrandViewModel.CategoryBrand =await _context.CategoryBrands.Include(c => c.Brand).Where(z=>z.Brand.BrandStatus==true).Include(c => c.Category).Where(z=>z.Category.CategoryStatus==true).ToListAsync();
+            categoryBrandViewModel.Brand = await _context.CategoryBrands.Include(x => x.Brand).Where(y => y.Brand.BrandStatus == true).ToListAsync();
+
+            categoryBrandViewModel.Category = await _context.CategoryBrands.Include(x => x.Category).Where(y => y.Category.CategoryStatus == true).ToListAsync();
+            return View(categoryBrandViewModel);
         }
 
-        // GET: CategoryBrand/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var categoryBrand = await _context.CategoryBrands
-                .Include(c => c.Brand)
-                .Include(c => c.Category)
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (categoryBrand == null)
-            {
-                return NotFound();
-            }
-
-            return View(categoryBrand);
-        }
-
-        // GET: CategoryBrand/Create
-        public IActionResult Create()
-        {
-            ViewData["BrandId"] = new SelectList(_context.Brands, "PK_BRAND_ID", "PK_BRAND_ID");
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "PK_CATEGORY_ID", "PK_CATEGORY_ID");
-            return View();
-        }
-
-        // POST: CategoryBrand/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,CategoryId,BrandId")] CategoryBrand categoryBrand)
+        public async Task<IActionResult> Index(int id)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(categoryBrand);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "PK_BRAND_ID", "PK_BRAND_ID", categoryBrand.BrandId);
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "PK_CATEGORY_ID", "PK_CATEGORY_ID", categoryBrand.CategoryId);
-            return View(categoryBrand);
+            categoryBrandViewModel.Category = await _context.CategoryBrands.Include(x => x.Category).Where(y => y.Category.CategoryStatus == true).ToListAsync();
+            categoryBrandViewModel.Brand = await _context.CategoryBrands.Include(x => x.Brand).Where(y => y.BrandId == id &&  y.Brand.BrandStatus == true).ToListAsync();
+      
+            return View(categoryBrandViewModel);
         }
 
-        // GET: CategoryBrand/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var categoryBrand = await _context.CategoryBrands.FindAsync(id);
-            if (categoryBrand == null)
-            {
-                return NotFound();
-            }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "PK_BRAND_ID", "PK_BRAND_ID", categoryBrand.BrandId);
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "PK_CATEGORY_ID", "PK_CATEGORY_ID", categoryBrand.CategoryId);
-            return View(categoryBrand);
-        }
-
-        // POST: CategoryBrand/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,CategoryId,BrandId")] CategoryBrand categoryBrand)
-        {
-            if (id != categoryBrand.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(categoryBrand);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryBrandExists(categoryBrand.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "PK_BRAND_ID", "PK_BRAND_ID", categoryBrand.BrandId);
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "PK_CATEGORY_ID", "PK_CATEGORY_ID", categoryBrand.CategoryId);
-            return View(categoryBrand);
-        }
-
-        // GET: CategoryBrand/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var categoryBrand = await _context.CategoryBrands
-                .Include(c => c.Brand)
-                .Include(c => c.Category)
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (categoryBrand == null)
-            {
-                return NotFound();
-            }
-
-            return View(categoryBrand);
-        }
-
-        // POST: CategoryBrand/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var categoryBrand = await _context.CategoryBrands.FindAsync(id);
-            _context.CategoryBrands.Remove(categoryBrand);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryBrandExists(int id)
-        {
-            return _context.CategoryBrands.Any(e => e.ID == id);
-        }
     }
 }
---
+++ SikayetTakipSitesi/SikayetTakipSitesi/ViewModels/CategoryBrandModelView.cs
@@ -0,0 +1,14 @@
+﻿using SikayetTakipSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SikayetTakipSitesi.ViewModels
+{
+    public class CategoryBrandModelView
+    {
+        public List<CategoryBrand> Category { get; set; }
+        public List<CategoryBrand> Brand { get; set; }
+    }
+}
