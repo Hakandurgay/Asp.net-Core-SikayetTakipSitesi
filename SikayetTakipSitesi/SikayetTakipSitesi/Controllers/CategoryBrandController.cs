19ceeb9ad88ba31988a5e5b7006946c64c50babe
Hakan Durgay <hakandurgay@gmail.com>
2020-12-13 04:46:46 +0300

    kategorileri listeleme yaparken tekrar eden veriler düzeltildi, kategori viewcomponent eklendi, sorgu iyileştirmeleri yapıldı

--- SikayetTakipSitesi/SikayetTakipSitesi/Controllers/CategoryBrandController.cs
+++ SikayetTakipSitesi/SikayetTakipSitesi/Controllers/CategoryBrandController.cs
@@ -28,19 +28,24 @@ namespace SikayetTakipSitesi.Controllers
         public async Task<IActionResult> Index()
         {
             //   categoryBrandViewModel.CategoryBrand =await _context.CategoryBrands.Include(c => c.Brand).Where(z=>z.Brand.BrandStatus==true).Include(c => c.Category).Where(z=>z.Category.CategoryStatus==true).ToListAsync();
-            categoryBrandViewModel.Brand = await _context.CategoryBrands.Include(x => x.Brand).Where(y => y.Brand.BrandStatus == true).ToListAsync();
-
-            categoryBrandViewModel.Category = await _context.CategoryBrands.Include(x => x.Category).Where(y => y.Category.CategoryStatus == true).ToListAsync();
-            return View(categoryBrandViewModel);
+            List<Brand> brands = await _context.Brands.Where(b => b.BrandStatus == true).ToListAsync();
+            return View(brands);
         }
 
         [HttpPost]
         public async Task<IActionResult> Index(int id)
         {
-            categoryBrandViewModel.Category = await _context.CategoryBrands.Include(x => x.Category).Where(y => y.Category.CategoryStatus == true).ToListAsync();
-            categoryBrandViewModel.Brand = await _context.CategoryBrands.Include(x => x.Brand).Where(y => y.BrandId == id &&  y.Brand.BrandStatus == true).ToListAsync();
-      
-            return View(categoryBrandViewModel);
+            //categoryBrandViewModel.Category = await _context.CategoryBrands.Include(x => x.Category).Where(y => y.Category.CategoryStatus == true).ToListAsync();
+            //categoryBrandViewModel.Brand = await _context.CategoryBrands.Include(x => x.Brand).Where(y => y.BrandId == id &&  y.Brand.BrandStatus == true).ToListAsync();
+
+            //return View(categoryBrandViewModel);
+            List<CategoryBrand> categoryBrands = await _context.CategoryBrands.Include(cb => cb.Brand).Where(y => y.Category.CategoryStatus == true && y.CategoryId == id).ToListAsync();
+            List<Brand> brands = new List<Brand>();
+            foreach (CategoryBrand categoryBrand in categoryBrands)  //viewmodel yapıp categorybrandi viewa yollayıp orada tekrar foreach ile içinde geziceğimiz için performans arttırmak için burada yani viewa yollamadan ayrılıp gönderiliyor
+            {
+                brands.Add(categoryBrand.Brand);
+            }
+            return View(brands);
         }
 
     }
