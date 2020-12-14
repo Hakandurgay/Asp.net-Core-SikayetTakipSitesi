51367fb28030a79eb7d94c58603cb1817f8f6d79
esraunal <47426593+unalesra@users.noreply.github.com>
2020-12-14 15:33:13 +0300

    Marka detay sayfası oluşturuldu

---
+++ SikayetTakipSitesi/SikayetTakipSitesi/ViewModels/BrandDetailModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SikayetTakipSitesi.Data;
+using SikayetTakipSitesi.Models;
+
+namespace SikayetTakipSitesi.ViewModels
+{
+    public class BrandDetailModelView
+    {
+        public List<Complaint> Complaints { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
